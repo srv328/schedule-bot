3f3ed3ac378559edc77010d0b5d2a9bf860f449a
srv328 <shevelevroman12@gmail.com>
2024-02-21 15:23:23 +1000

    handling added buttons

--- routers/admin_panel.py
+++ routers/admin_panel.py
@@ -1,11 +1,11 @@
-from aiogram.filters import Command
+from os import path
 from aiogram import Router, F
 from aiogram.types import Message, FSInputFile
-from config import admins
-from utils import bot
+from config import admins, database_path, log_path
+from utils import get_formatted_date
 from aiogram.enums.parse_mode import ParseMode
-from keyboards import clear_button, schedule_markup
-from work_with_db import has_schedule, execute_query
+from keyboards import admin_actions
+from aiogram.exceptions import TelegramBadRequest
 
 router = Router()
 
@@ -17,4 +17,49 @@ async def admin_panel(message: Message):
         await message.answer('<b>Куда мы лезем?</b>', parse_mode=ParseMode.HTML)
         await message.answer_video(gif)
         return
-    await message.answer("<b>Что делать то будем?</b>", parse_mode=ParseMode.HTML)
+    await message.answer("<b>Что делать то будем?</b>", parse_mode=ParseMode.HTML, reply_markup=admin_actions)
+
+
+@router.message(F.text == "Начать рассылку📖")
+async def start_sending(message: Message):
+    if str(message.from_user.id) not in admins:
+        gif = FSInputFile("media/admin.mp4")
+        await message.answer('<b>Куда мы лезем?</b>', parse_mode=ParseMode.HTML)
+        await message.answer_video(gif)
+        return
+    pass
+
+
+@router.message(F.text == "Выгрузить БД🗄")
+async def export_database(message: Message):
+    if str(message.from_user.id) not in admins:
+        gif = FSInputFile("media/admin.mp4")
+        await message.answer('<b>Куда мы лезем?</b>', parse_mode=ParseMode.HTML)
+        await message.answer_video(gif)
+        return
+    if path.exists(database_path):
+        try:
+            db = FSInputFile(database_path, f"db_{get_formatted_date()}.db")
+            await message.answer_document(db)
+        except TelegramBadRequest:
+            await message.answer("<b>БД пустая!</b>", parse_mode=ParseMode.HTML)
+    else:
+        await message.answer("<b>Не удалось выгрузить БД!</b>", parse_mode=ParseMode.HTML)
+
+
+@router.message(F.text == "Выгрузить лог🗒")
+async def export_database(message: Message):
+    if str(message.from_user.id) not in admins:
+        gif = FSInputFile("media/admin.mp4")
+        await message.answer('<b>Куда мы лезем?</b>', parse_mode=ParseMode.HTML)
+        await message.answer_video(gif)
+        return
+    if path.exists(log_path):
+        try:
+            log = FSInputFile(log_path, f"log_{get_formatted_date()}.log")
+            await message.answer_document(log)
+        except TelegramBadRequest:
+            await message.answer("<b>Лог пустой!</b>", parse_mode=ParseMode.HTML)
+    else:
+        await message.answer("<b>Не удалось выгрузить лог!</b>", parse_mode=ParseMode.HTML)
+
