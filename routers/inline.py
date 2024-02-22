ab6eb7e853b017828d768f3ed2b08bf1a3d963c9
srv328 <shevelevroman12@gmail.com>
2024-02-22 15:19:20 +1000

    add a new InlineQueryArticle to share default link

--- routers/inline.py
+++ routers/inline.py
@@ -30,25 +30,40 @@ async def default(query: InlineQuery):
     for old, new in replacements:
         base_message = base_message.replace(old, new)
 
-    schedule_result = [InlineQueryResultArticle(
-        id='1',
-        title="Поделиться расписанием",
-        description="Нажмите, чтобы отправить ваше расписание",
-        input_message_content=InputTextMessageContent(
-            message_text=f"Используй моё расписание: <a href='https://t.me/FEFUDVFU_bot?start={user_id}'>тык</a>",
-            parse_mode=ParseMode.HTML
+    schedule_result = [
+        InlineQueryResultArticle(
+            id='1',
+            title="Поделиться ссылкой на бота",
+            description="Нажмите, чтобы поделиться ссылкой",
+            input_message_content=InputTextMessageContent(
+                message_text=f"Настрой своё расписание в несколько кликов! "
+                             f"<a href='https://t.me/FEFUDVFU_bot'>@FEFUDVFU_bot</a>",
+                parse_mode=ParseMode.HTML
+            ),
+            thumb_url="https://sun1-83.userapi.com/s/v1/ig2/GAG4M-nEEH2uw43iclrQMZNG-_8vRfm23f_Ol5Tv-xerA8bTF94BMIXTVhB"
+                      "km41UV2Fpo3iUN2Jh6hljOEPWl7VR.jpg?size=100x100&quality=95&crop=0,0,1080,1080&ava=1"
         ),
-        thumb_url="https://e7.pngegg.com/pngimages/529/122/png-clipart-computer-icons-calendar-"
-                  "date-google-calendar-calendar-icon-purple-violet.png"
-    ), InlineQueryResultArticle(
-        id='2',
-        title="Поделиться статистикой",
-        description="Нажмите, чтобы отправить вашу статистику",
-        input_message_content=InputTextMessageContent(
-            message_text=base_message,
-            parse_mode=ParseMode.HTML
+        InlineQueryResultArticle(
+            id='2',
+            title="Поделиться расписанием",
+            description="Нажмите, чтобы отправить ваше расписание",
+            input_message_content=InputTextMessageContent(
+                message_text=f"Используй моё расписание: <a href='https://t.me/FEFUDVFU_bot?start={user_id}'>тык</a>",
+                parse_mode=ParseMode.HTML
+            ),
+            thumb_url="https://e7.pngegg.com/pngimages/529/122/png-clipart-computer-icons-calendar-"
+                      "date-google-calendar-calendar-icon-purple-violet.png"
         ),
-        thumb_url="https://ladys-room.ru/wp-content/uploads/2022/09/086bcbc460826f25e9de443db73fc4c0-429x425.png"
-    )]
+        InlineQueryResultArticle(
+            id='3',
+            title="Поделиться статистикой",
+            description="Нажмите, чтобы отправить вашу статистику",
+            input_message_content=InputTextMessageContent(
+                    message_text=base_message,
+                    parse_mode=ParseMode.HTML
+                ),
+            thumb_url="https://ladys-room.ru/wp-content/uploads/2022/09/086bcbc460826f25e9de443db73fc4c0-429x425.png"
+        )
+    ]
 
     await query.answer(schedule_result, cache_time=10, is_personal=True)
